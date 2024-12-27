a557418742f239782790e2895fffa372ec2c698c
Marcus Nogueira <marcusnogueiraa@gmail.com>
2024-12-26 21:53:22 -0300

    init project

---
+++ src/main/java/com/concord/concordapi/ConcordapiApplication.java
@@ -0,0 +1,13 @@
+package com.concord.concordapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcordapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcordapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/concord/concordapi/ConcordapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.concord.concordapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcordapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
