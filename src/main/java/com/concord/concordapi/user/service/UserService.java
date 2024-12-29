8962318f2e0c05a620f8575f834c4caa0a95a00a
Marcus Nogueira <marcusnogueiraa@gmail.com>
2024-12-29 18:07:27 -0300

    fix: entity not found message fixed

--- src/main/java/com/concord/concordapi/user/service/UserService.java
+++ src/main/java/com/concord/concordapi/user/service/UserService.java
@@ -3,9 +3,9 @@ package com.concord.concordapi.user.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.concord.concordapi.shared.exception.EntityNotFoundException;
 import com.concord.concordapi.user.dto.UserRequestDto;
 import com.concord.concordapi.user.entity.User;
-import com.concord.concordapi.user.exception.UserNotFoundException;
 import com.concord.concordapi.user.mapper.UserMapper;
 import com.concord.concordapi.user.repository.UserRepository;
 
@@ -17,7 +17,7 @@ public class UserService {
 
     public UserRequestDto getByUsername(String username){
         User user = userRepository.findByUsername(username)
-                .orElseThrow(() -> new UserNotFoundException("User "+username+" not found."));
+                .orElseThrow(() -> new EntityNotFoundException("User "+username+" not found."));
         return UserMapper.toDto(user);
     }
 }
