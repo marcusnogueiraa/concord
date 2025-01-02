a2ebf8ed51f35e956ed999491e46e70231c29ac0
sheiely <sheielynascimento@gmail.com>
2025-01-02 18:06:08 -0300

    test: refact test to not use mock

--- src/test/java/com/concord/concordapi/server/e2e/controller/ServerControllerTest.java
+++ src/test/java/com/concord/concordapi/server/e2e/controller/ServerControllerTest.java
@@ -8,19 +8,20 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.web.client.RestTemplate;
 
-import com.concord.concordapi.server.e2e.responses.ServerExpectedDTO;
-import com.concord.concordapi.server.e2e.responses.UserExpectedDTO;
-import com.concord.concordapi.shared.UtilsMethods;
 import com.concord.concordapi.shared.config.SecurityConfiguration;
+import com.concord.concordapi.shared.response.ServerExpectedDTO;
+import com.concord.concordapi.shared.response.UserExpectedDTO;
+import com.concord.concordapi.shared.util.UtilsMethods;
 import com.concord.concordapi.user.entity.User;
 import com.concord.concordapi.user.repository.UserRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -28,15 +29,16 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 @ActiveProfiles("test")
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-@SpringBootTest
-@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ServerControllerTest {
-    @Autowired
-    private MockMvc mockMvc;
     @Autowired
     private SecurityConfiguration securityConfiguration;
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private RestTemplate restTemplate;
+    @LocalServerPort
+    private int port;
 
     private User testUser;
     private String token;
@@ -44,81 +46,94 @@ public class ServerControllerTest {
 
     @BeforeEach
     public void setup() throws Exception {
-        testUser = new User(null, "user"+iterator, "user"+iterator, "user"+iterator+"@gmail.com", securityConfiguration.passwordEncoder().encode("123456"), null, null, null);
+        testUser = new User(null, "user" + iterator, "user" + iterator, "user" + iterator + "@gmail.com", securityConfiguration.passwordEncoder().encode("123456"), null, null, null, null);
         testUser = userRepository.save(testUser);
-        String jsonRequest = "{ \"username\": \""+testUser.getUsername()+"\", \"password\": \"123456\" }";
-        token = mockMvc.perform(MockMvcRequestBuilders.post("/api/auth/login")
-                .contentType(MediaType.APPLICATION_JSON) // Definindo o tipo de conteúdo como JSON
-                .content(jsonRequest))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andReturn().getResponse().getContentAsString();
-        token = UtilsMethods.extractFromResponse(token);
+        String jsonRequest = "{ \"username\": \"" + testUser.getUsername() + "\", \"password\": \"123456\" }";
+        
+        ResponseEntity<String> response = restTemplate.exchange(
+                "http://localhost:"+port+"/api/auth/login",
+                HttpMethod.POST,
+                new HttpEntity<>(jsonRequest, UtilsMethods.createJsonHeaders()),
+                String.class);
+
+        token = UtilsMethods.extractFromResponse(response.getBody());
         iterator++;
     }
 
     @Test
     public void testCreateServer() throws Exception {
-        ServerExpectedDTO actualResponse = UtilsMethods.createServer(mockMvc, token, testUser, "Server 1");
-        ServerExpectedDTO expectedResponse = new ServerExpectedDTO(actualResponse.id(), "Server 1", new UserExpectedDTO(testUser.getName(), testUser.getUsername(), testUser.getEmail(), null), List.of());
+        ServerExpectedDTO actualResponse = UtilsMethods.createServer(restTemplate, port, token, testUser, "Server 1");
+        ServerExpectedDTO expectedResponse = new ServerExpectedDTO(actualResponse.id(), "Server 1", new UserExpectedDTO(testUser.getId(), testUser.getName(), testUser.getUsername(), testUser.getEmail(), testUser.getImagePath(), null), null, List.of());
         assertEquals(expectedResponse, actualResponse);
 
     }
 
     @Test
     public void testGetServerById() throws Exception {
-        ServerExpectedDTO actualServer = UtilsMethods.createServer(mockMvc, token, testUser, "Server 2");
-
-        String response = mockMvc.perform(MockMvcRequestBuilders.get("/api/servers/"+actualServer.id())
-                .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(""))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andReturn().getResponse().getContentAsString();  
-        response = response.replaceAll("\"createdAt\":\"[^\"]*\"", "\"createdAt\":null");
+        ServerExpectedDTO actualServer = UtilsMethods.createServer(restTemplate, port, token, testUser, "Server 2");
+        HttpHeaders headers = UtilsMethods.createJsonHeaders();
+        headers.setBearerAuth(token);
+        ResponseEntity<String> responseEntity = restTemplate.exchange(
+            "http://localhost:" + port + "/api/servers/" + actualServer.id(),
+            HttpMethod.GET,
+            new HttpEntity<>(null, headers),
+            String.class
+        );
         ObjectMapper objectMapper = new ObjectMapper();
-        ServerExpectedDTO actualResponse = objectMapper.readValue(response, ServerExpectedDTO.class);
-        ServerExpectedDTO expectedResponse = new ServerExpectedDTO(actualResponse.id(), "Server 2", new UserExpectedDTO(testUser.getName(), testUser.getUsername(), testUser.getEmail(), null), List.of());
+        String responseBody = responseEntity.getBody().replaceAll("\"createdAt\":\"[^\"]*\"", "\"createdAt\":null");
+        ServerExpectedDTO actualResponse = objectMapper.readValue(responseBody, ServerExpectedDTO.class);
+        ServerExpectedDTO expectedResponse = new ServerExpectedDTO(actualResponse.id(), "Server 2", new UserExpectedDTO(testUser.getId(), testUser.getName(), testUser.getUsername(), testUser.getEmail(), null, null), null, List.of());
         assertEquals(expectedResponse, actualResponse);
         
     }
 
     @Test
     public void testUpdateServer() throws Exception {
-        ServerExpectedDTO actualServer = UtilsMethods.createServer(mockMvc, token, testUser, "Server 3");
+        ServerExpectedDTO actualServer = UtilsMethods.createServer(restTemplate, port, token, testUser, "Server 3");
         ObjectMapper objectMapper = new ObjectMapper();
         String jsonContent = "{\"name\":\"Server 3 modified\"}";
-        String response = mockMvc.perform(MockMvcRequestBuilders.put("/api/servers/"+actualServer.id())
-                .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(jsonContent))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andReturn().getResponse().getContentAsString();  
+        HttpHeaders headers = UtilsMethods.createJsonHeaders();
+        headers.setBearerAuth(token);
+        ResponseEntity<String> responseEntity = restTemplate.exchange(
+            "http://localhost:" + port + "/api/servers/" + actualServer.id(),
+            HttpMethod.PUT,
+            new HttpEntity<>(jsonContent, headers),
+            String.class
+        );
 
-        response = response.replaceAll("\"createdAt\":\"[^\"]*\"", "\"createdAt\":null");
-        ServerExpectedDTO actualResponse = objectMapper.readValue(response, ServerExpectedDTO.class);
-        ServerExpectedDTO expectedResponse = new ServerExpectedDTO(actualResponse.id(), "Server 3 modified", new UserExpectedDTO(testUser.getName(), testUser.getUsername(), testUser.getEmail(), null), List.of());
+        String responseBody = responseEntity.getBody().replaceAll("\"createdAt\":\"[^\"]*\"", "\"createdAt\":null");
+        ServerExpectedDTO actualResponse = objectMapper.readValue(responseBody, ServerExpectedDTO.class);
+        ServerExpectedDTO expectedResponse = new ServerExpectedDTO(actualResponse.id(), "Server 3 modified", new UserExpectedDTO(testUser.getId(), testUser.getName(), testUser.getUsername(), testUser.getEmail(), null, null), null, List.of());
         assertEquals(expectedResponse, actualResponse);
     }
 
     @Test
     public void testSubscribeServer() throws Exception {
-        ServerExpectedDTO actualServer = UtilsMethods.createServer(mockMvc, token, testUser, "Server 4");
+        ServerExpectedDTO actualServer = UtilsMethods.createServer(restTemplate, port, token, testUser, "Server 4");
 
-        mockMvc.perform(MockMvcRequestBuilders.post("/api/servers/"+actualServer.id()+"/subscribe/"+testUser.getUsername())
-                .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(""))
-                .andExpect(MockMvcResultMatchers.status().isCreated());
+        HttpHeaders headers = UtilsMethods.createJsonHeaders();
+        headers.setBearerAuth(token);
+        ResponseEntity<String> responseEntity = restTemplate.exchange(
+            "http://localhost:" + port + "/api/servers/" + actualServer.id()+"/subscribe/"+testUser.getUsername(),
+            HttpMethod.POST,
+            new HttpEntity<>(headers),
+            String.class
+        );
+        assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
     }
 
     @Test
     public void testDeleteServer() throws Exception {
-        ServerExpectedDTO actualServer = UtilsMethods.createServer(mockMvc, token, testUser, "Server 5");
-        mockMvc.perform(MockMvcRequestBuilders.delete("/api/servers/"+actualServer.id())
-                .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(""))
-                .andExpect(MockMvcResultMatchers.status().isNoContent());
+        ServerExpectedDTO actualServer = UtilsMethods.createServer(restTemplate, port, token, testUser, "Server 5");
+        HttpHeaders headers = UtilsMethods.createJsonHeaders();
+        headers.setBearerAuth(token);
+        ResponseEntity<String> responseEntity = restTemplate.exchange(
+            "http://localhost:" + port + "/api/servers/" + actualServer.id(),
+            HttpMethod.DELETE,
+            new HttpEntity<>(headers),
+            String.class
+        );
+        assertEquals(HttpStatus.NO_CONTENT, responseEntity.getStatusCode());
     }
     
 }
--- src/test/java/com/concord/concordapi/shared/response/UserExpectedDTO.java
+++ src/test/java/com/concord/concordapi/shared/response/UserExpectedDTO.java
@@ -1,9 +1,11 @@
-package com.concord.concordapi.server.e2e.responses;
+package com.concord.concordapi.shared.response;
 
 
 public record UserExpectedDTO (
+    Long id,
     String name,
     String username,
     String email,
+    String imagePath,
     String createdAt
 ) {}
