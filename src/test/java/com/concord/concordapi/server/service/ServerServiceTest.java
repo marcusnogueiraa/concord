2bfd0eb29d7c8c324e9157a5b04cac86378aaacc
sheiely <sheielynascimento@gmail.com>
2025-01-22 22:55:43 -0300

    test: add unit tests to Server

--- src/test/java/com/concord/concordapi/server/service/ServerServiceTest.java
+++ src/test/java/com/concord/concordapi/server/service/ServerServiceTest.java
@@ -1,13 +1,15 @@
-package com.concord.concordapi.server.unit.service;
+package com.concord.concordapi.server.service;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Optional;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -22,7 +24,6 @@ import com.concord.concordapi.server.dto.request.ServerPutBodyDTO;
 import com.concord.concordapi.server.dto.response.ServerDto;
 import com.concord.concordapi.server.entity.Server;
 import com.concord.concordapi.server.repository.ServerRepository;
-import com.concord.concordapi.server.service.ServerService;
 import com.concord.concordapi.shared.exception.EntityNotFoundException;
 import com.concord.concordapi.user.entity.User;
 import com.concord.concordapi.user.repository.UserRepository;
@@ -51,6 +52,7 @@ class ServerServiceTest {
         Server server = new Server();
         server.setId(serverId);
         server.setName("Test Server");
+        server.setOwner(new User(1L, "test user", "testuser", "test@gmail.com", "123", null, null, null, null));
 
         Mockito.when(serverRepository.findById(serverId)).thenReturn(Optional.of(server));
 
@@ -73,17 +75,15 @@ class ServerServiceTest {
     @Test
     void testCreateServer() {
         ServerCreateBodyDTO serverDto = new ServerCreateBodyDTO("TestServer", 1L, "tempImagePath");
-        User owner = new User();
-        owner.setId(1L);
-
+        User owner = new User(1L, "test user", "testuser", "test@gmail.com", "123", null, null, null, null);
+        owner.setServers(new ArrayList());
         Mockito.when(userRepository.findById(serverDto.ownerId())).thenReturn(Optional.of(owner));
         Mockito.doNothing().when(authService).isUserTheAuthenticated(owner);
         Mockito.when(serverRepository.save(Mockito.any(Server.class))).thenAnswer(i -> i.getArgument(0));
 
         ServerDto result = serverService.create(serverDto);
-
         assertNotNull(result);
-        assertEquals("Test Server", result.name());
+        assertEquals("TestServer", result.name());
         Mockito.verify(fileStorageService).persistImage(Mockito.any(FilePrefix.class), Mockito.eq("tempImagePath"));
         Mockito.verify(serverRepository).save(Mockito.any(Server.class));
     }
@@ -97,6 +97,7 @@ class ServerServiceTest {
         User owner = new User();
         owner.setId(1L);
         server.setOwner(owner);
+        server.setUsers(new HashSet<>());
 
         Mockito.when(serverRepository.findById(serverId)).thenReturn(Optional.of(server));
         Mockito.doNothing().when(authService).isUserTheAuthenticated(owner);
@@ -115,6 +116,7 @@ class ServerServiceTest {
     void testUpdateById() {
         Long serverId = 1L;
         ServerPutBodyDTO serverDto = new ServerPutBodyDTO("Updated Server", "newTempImagePath");
+
         Server server = new Server();
         server.setId(serverId);
         server.setName("Old Server");
@@ -125,7 +127,7 @@ class ServerServiceTest {
         Mockito.when(serverRepository.findById(serverId)).thenReturn(Optional.of(server));
         Mockito.doNothing().when(authService).isUserTheAuthenticated(owner);
         Mockito.when(fileStorageService.fileExists(server.getImagePath())).thenReturn(true);
-        Mockito.doNothing().when(fileStorageService).persistImage(Mockito.any(FilePrefix.class), Mockito.eq("newTempImagePath"));
+        Mockito.when(fileStorageService.persistImage(Mockito.any(FilePrefix.class), Mockito.eq("newTempImagePath"))).thenReturn("newTempImagePath");
         Mockito.doNothing().when(fileStorageService).deleteFile(server.getImagePath());
         Mockito.when(serverRepository.save(Mockito.any(Server.class))).thenReturn(server);
 
@@ -144,6 +146,7 @@ class ServerServiceTest {
         Long serverId = 1L;
 
         User user = new User();
+        user.setServers(new ArrayList<>());
         user.setUsername(username);
         Server server = new Server();
         server.setId(serverId);
